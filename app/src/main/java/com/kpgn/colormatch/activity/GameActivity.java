7788cd5e9915d09412aecec2eb576a3dbda67290
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-12-06 21:06:22 +0530

    Implement game timer and restart.

--- app/src/main/java/com/kpgn/colormatch/activity/GameActivity.java
+++ app/src/main/java/com/kpgn/colormatch/activity/GameActivity.java
@@ -1,12 +1,33 @@
 package com.kpgn.colormatch.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kpgn.colormatch.R;
+import com.kpgn.colormatch.constant.ApplicationConstant;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameActivity extends BaseActivity {
+
+    @BindView(R.id.cta_restart)
+    ImageView mRestart;
+
+    @BindView(R.id.tv_counter)
+    TextView mCounter;
+
+    @BindView(R.id.question_container)
+    View mQuestionContainer;
+
+    @BindView(R.id.img_answer_notification)
+    ImageView mAnswerNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +40,7 @@ public class GameActivity extends BaseActivity {
         super.onResume();
         ButterKnife.bind(this);
 
-        setupView();
+        showTimerAndStartGame();
     }
 
     @Override
@@ -27,9 +48,72 @@ public class GameActivity extends BaseActivity {
         super.onPause();
     }
 
-    private void setupView() {
-
+    private void showTimerAndStartGame() {
+        toggleQuestionView(true);
+        mCounter.setText("3");
+        final Handler handler = new Handler();
+        final AtomicInteger atomicInteger = new AtomicInteger(ApplicationConstant.TIMER_COUNT);
+        final Runnable counter = new Runnable() {
+            @Override
+            public void run() {
+                mCounter.setText(Integer.toString(atomicInteger.get()));
+                if (atomicInteger.getAndDecrement() >= 1)
+                    handler.postDelayed(this, ApplicationConstant.MILLI_SECOND);
+                else {
+                    setupQuestions();
+                }
+            }
+        };
+        handler.postDelayed(counter, ApplicationConstant.MILLI_SECOND);
     }
 
+    private void setupQuestions() {
+        toggleQuestionView(false);
+        getNewQuestion();
+    }
+
+    private void getNewQuestion() {
+        //mAnswerNotification.setVisibility(View.INVISIBLE);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.cta_restart)
+    public void restartGame(View view) {
+        showTimerAndStartGame();
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.cta_yes)
+    public void optionYes(View view) {
+        toggleAnswerNotification(true);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.cta_no)
+    public void optionNo(View view) {
+        toggleAnswerNotification(false);
+    }
+
+    private void toggleQuestionView(boolean isHidden) {
+        if (isHidden) {
+            mCounter.setVisibility(View.VISIBLE);
+            mRestart.setVisibility(View.GONE);
+            mQuestionContainer.setVisibility(View.GONE);
+            mAnswerNotification.setVisibility(View.INVISIBLE);
+        } else {
+            mCounter.setVisibility(View.GONE);
+            mRestart.setVisibility(View.VISIBLE);
+            mQuestionContainer.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void toggleAnswerNotification(boolean isCorrect) {
+        mAnswerNotification.setVisibility(View.VISIBLE);
+        if (isCorrect) {
+            mAnswerNotification.setImageDrawable(getResources().getDrawable(R.drawable.ic_correct));
+        } else {
+            mAnswerNotification.setImageDrawable(getResources().getDrawable(R.drawable.ic_wrong));
+        }
+    }
 
 }
--- app/src/main/java/com/kpgn/colormatch/constant/ApplicationConstant.java
+++ app/src/main/java/com/kpgn/colormatch/constant/ApplicationConstant.java
@@ -4,4 +4,7 @@ public abstract class ApplicationConstant {
 
     public static final String APP_ID = "com.kpgn.colormatch";
 
+    public static final int TIMER_COUNT = 2;
+    public static final long MILLI_SECOND = 1000;
+
 }
